ad31389267a1e3d62598282cd33c23f956a9e88d
Kevin Brill <kevin.brill@gmail.com>
2018-05-30 18:54:57 -0400

    * Added support for generating MTL files with a texture path formatted for web URLs * Code refactoring on the OBJExporter

--- Assets/ExportModelData.cs
+++ Assets/ExportModelData.cs
@@ -8,6 +8,7 @@ public class ExportModelData
     public static string outputDirectory = @"L:\rift\converted";
     public static string outputDirectoryTextures = @"textures";
     public static string expectedTextureExtension = "jpg";
+    public static bool mtlWebPathing = false;
     public static HashSet<int> langIDs = new HashSet<int>();
 
 }
